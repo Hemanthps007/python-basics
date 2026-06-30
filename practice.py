11ddcfd6220703b909b1be07d72b31a4caaee4e9
Hemanth p s <pshemanth2@gmail.com>
2026-06-30 22:49:05 +0530

    revised Loops

--- practice.py
+++ practice.py
@@ -286,5 +286,31 @@
 
 # print(is_valid_password("1234567"))
 
+# n = 5
+# for i in range (n):
+#     for j in range(i+1):
+#         print("*",end = " ")
+#     print()
+    
+# n = int(input("Enter the number: "))
+# i = 0
+# while i<=n:
+#     j = 1
+#     while j <= i:
+#         print("*",end= " ")
+#         j += 1
+#     print()
+#     i += 1
+
+# n = 5
+# for i in range(n,0,-1):
+#     for j in range (i):
+#         print("1",end = " ")
+#     print()
 
 
+n = 6
+for i in range(1,6):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
